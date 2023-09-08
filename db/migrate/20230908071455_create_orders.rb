23697e696fe298e7b5e41dc9a26dea7666f16912
Kazuki ARAI <zukain5@gmail.com>
2023-09-08 16:18:21 +0900

    create table (#1)
    * create tables
    
    * rails db:migrate

---
+++ db/migrate/20230908071455_create_orders.rb
@@ -0,0 +1,11 @@
+class CreateOrders < ActiveRecord::Migration[7.0]
+  def change
+    create_table :orders do |t|
+      t.integer :total_points
+      t.references :user, null: false, foreign_key: true
+      t.references :store, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
