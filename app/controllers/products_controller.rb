5bc2568d2b4cf6a96d813aa9f777a5c281565e75
Kazuki ARAI <zukain5@gmail.com>
2023-09-08 17:04:05 +0900

    impl show (#4)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,15 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
-    render status: :ok, json: { products: @products }
+    render status: :ok, json: @products
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    if @product.nil?
+      render status: :not_found, json: {}
+    else
+      render status: :ok, json: @product
+    end
   end
 end
