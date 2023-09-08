3eb102c05837aeca1b6bd263ce493687d3ac06a2
Kazuki ARAI <zukain5@gmail.com>
2023-09-08 17:57:18 +0900

    add POST /orders

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # root "articles#index"
   resources :products
   resources :users
+  resources :orders
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,10 +15,19 @@ users = %w[
   田中次郎
 ]
 
+stores = %w[
+  S珈琲 恵比寿店
+  M&W 恵比寿店
+]
+
 products.each do |name, price|
   Product.create(name:, price:)
 end
 
 users.each do |name|
   User.create(name:)
+end
+
+stores.each do |name|
+  Store.create(name:)
 end
