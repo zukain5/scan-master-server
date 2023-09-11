81c943e6c9813a3a787f87d8c800fc8cd049eb40
Kazuki ARAI <zukain5@gmail.com>
2023-09-11 17:58:58 +0900

    allow null user id (#13)
    * complete it
    
    * format code

--- app/controllers/link_controller.rb
+++ app/controllers/link_controller.rb
@@ -3,7 +3,7 @@ class LinkController < ApplicationController
     order = Order.find(params[:order_id])
     user = User.find(params[:user_id])
 
-    return render status: :conflict, json: { 'error': 'The order is already linked with a certain user.' } if order.user
+    # return render status: :conflict, json: { 'error': 'The order is already linked with a certain user.' } if order.user
 
     if order.update(user:)
       render status: :ok, json: order
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,19 +14,19 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new(
+    order = Order.new(
       store_id: params[:store_id],
-      user_id: params[:user_id],
       total_points: params[:total_points],
     )
+    order.user_id = params[:user_id] if params[:user_id]
     order_items = params[:order_items].map do |item|
       {
         product_id: item[:product_id],
         quantity: item[:quantity],
       }
     end
-    @order.order_items.build(order_items)
-    @order.save
-    render status: :ok, json: @order, methods: :order_items
+    order.order_items.build(order_items)
+    order.save
+    render status: :ok, json: order, methods: :order_items
   end
 end
