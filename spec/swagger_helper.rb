4e892a023297e4f688b56ad4d7ecb1befccd54de
Kazuki ARAI <zukain5@gmail.com>
2023-09-11 15:11:33 +0900

    changed swagger host

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -19,7 +19,7 @@ RSpec.configure do |config|
       openapi: '3.0.1',
       info: {
         title: 'API V1',
-        version: 'v1'
+        version: 'v1',
       },
       paths: {},
       servers: [
@@ -27,7 +27,7 @@ RSpec.configure do |config|
           url: 'https://{defaultHost}',
           variables: {
             defaultHost: {
-              default: 'www.example.com'
+              default: ENV['API_HOST'],
             }
           }
         }
