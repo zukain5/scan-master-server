b6918086e0473fe763f877033880b417f90d63bc
Kazuki ARAI <zukain5@gmail.com>
2023-09-11 14:57:49 +0900

    add get users (#8)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render :ok, json: @users
+  end
+
   def show
     @user = User.find_by(id: params[:id])
     if @user
