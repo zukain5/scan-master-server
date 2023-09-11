4434dc4d13c6a103dd9f03cb67827459e35845ce
Kazuki ARAI <zukain5@gmail.com>
2023-09-11 17:19:03 +0900

    [POST /link] return conflict when already linked

--- app/controllers/link_controller.rb
+++ app/controllers/link_controller.rb
@@ -2,6 +2,9 @@ class LinkController < ApplicationController
   def create
     order = Order.find(params[:order_id])
     user = User.find(params[:user_id])
+
+    return render status: :conflict, json: { 'error': 'The order is already linked with a certain user.' } if order.user
+
     if order.update(user:)
       render status: :ok, json: order
     else
