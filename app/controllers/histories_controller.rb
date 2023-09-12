c95ac3c28f9709ca0b084f30207b915d5c49e1db
Kazuki ARAI <zukain5@gmail.com>
2023-09-12 15:18:33 +0900

    changed histories response to include order_items

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -2,6 +2,6 @@ class HistoriesController < ApplicationController
   def show
     user = User.find(params[:user_id])
     histories = Order.where(user:)
-    render status: :ok, json: { 'orders': histories }
+    render status: :ok, json: { 'orders': histories.as_json(include: :order_items) }
   end
 end
