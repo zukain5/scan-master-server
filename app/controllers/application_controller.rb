ae808d7936e5af0532136bead9d126305d81e3bd
Kazuki ARAI <zukain5@gmail.com>
2023-09-12 16:34:55 +0900

    add resuce for error404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :error404
+
+  def error404
+    render status: :not_found, nothing: true
+  end
 end
