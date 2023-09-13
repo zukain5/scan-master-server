64aa379c9a7eabcad3746890f5036bdf4c21f77a
Kazuki ARAI <zukain5@gmail.com>
2023-09-13 16:02:02 +0900

    changed recommend product from 3 to 4

--- app/controllers/recommends_controller.rb
+++ app/controllers/recommends_controller.rb
@@ -1,6 +1,6 @@
 class RecommendsController < ApplicationController
   def show
-    products = Product.where(is_ec: true).sample(3)
+    products = Product.where(is_ec: true).sample(4)
     render status: :ok, json: products
   end
 end
