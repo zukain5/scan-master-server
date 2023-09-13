cd517508cc6e43caec8fb8194c5b3e5ef5f31126
Kazuki ARAI <zukain5@gmail.com>
2023-09-13 14:35:38 +0900

    add duration_days to product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_13_051618) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_13_053001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_13_051618) do
     t.datetime "updated_at", null: false
     t.boolean "is_ec", default: false, null: false
     t.string "image_url"
+    t.integer "duration_days"
   end
 
   create_table "reminds", force: :cascade do |t|
