f80c4018d1f7219d870908d5ce7784352d29429f
Kazuki ARAI <zukain5@gmail.com>
2023-09-13 16:55:07 +0900

    rails db:migrate

--- db/migrate/20230913075349_create_reviews.rb
+++ db/migrate/20230913075349_create_reviews.rb
@@ -3,7 +3,7 @@ class CreateReviews < ActiveRecord::Migration[7.0]
     create_table :reviews do |t|
       t.references :product, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
-      t.boolean :like
+      t.boolean :like, null: false, default: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_13_060122) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_13_075349) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_13_060122) do
     t.index ["user_id"], name: "index_reminds_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "user_id", null: false
+    t.boolean "like", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_reviews_on_product_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "stores", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -72,4 +82,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_13_060122) do
   add_foreign_key "orders", "users"
   add_foreign_key "reminds", "products"
   add_foreign_key "reminds", "users"
+  add_foreign_key "reviews", "products"
+  add_foreign_key "reviews", "users"
 end
