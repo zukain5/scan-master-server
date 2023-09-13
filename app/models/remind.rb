09ef490659924ee2373661182e0c7ec1a5c67ed2
Kazuki ARAI <zukain5@gmail.com>
2023-09-13 14:46:10 +0900

    create remind records in orders#create

--- app/models/remind.rb
+++ app/models/remind.rb
@@ -1,4 +1,19 @@
 class Remind < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  class << self
+    def create_from_order(order)
+      products = order.order_items.map(&:product)
+      products.each do |product|
+        next unless product.duration_days
+
+        Remind.create(
+          user_id: order.user_id,
+          product_id: product.id,
+          remind_at: order.created_at + product.duration_days.days,
+        )
+      end
+    end
+  end
 end
