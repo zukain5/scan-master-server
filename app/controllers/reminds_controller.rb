c7133887f6fa9a667e36605584c1207a72324572
Kazuki ARAI <zukain5@gmail.com>
2023-09-13 15:52:08 +0900

    fix reminds#show

--- app/controllers/reminds_controller.rb
+++ app/controllers/reminds_controller.rb
@@ -3,12 +3,14 @@ class RemindsController < ApplicationController
     user = User.find(params[:user_id])
     items = Order.where(user:).map(&:order_items).flatten
     reminds = items.map do |item|
+      next unless item.product.duration_days
+
       Remind.new(
         user_id: user.id,
         product_id: item.product.id,
         remind_at: item.created_at + item.product.duration_days.days,
       )
-    end
+    end.compact
 
     render status: :ok, json: reminds
   end
