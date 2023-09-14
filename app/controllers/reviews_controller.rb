b0349687d0740d808ac4523f8f703e718be84aff
Kazuki ARAI <zukain5@gmail.com>
2023-09-14 10:47:09 +0900

    add review process

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,28 @@
 class ReviewsController < ApplicationController
-  def create
+  def index
+    user = User.find(params[:user_id])
+    product = Product.find(params[:product_id])
+    review = Review.find(user:, product:)
+    render status: :ok, json: review
+  end
+
+  def post
     user = User.find(params[:user_id])
     product = Product.find(params[:product_id])
     review = Review.find_by(user:, product:)
-    return update(review, params[:like]) if review
+    return create(user, product, params[:like]) if review.nil?
+
+    if params[:like].zero?
+      destroy(review)
+    else
+      update(review, params[:like])
+    end
+  end
+
+  private
+
+  def create(user, product, like)
+    return render status: :ok, nothing: true if like.zero?
 
     review = Review.new(
       user:,
@@ -17,13 +36,18 @@ class ReviewsController < ApplicationController
     end
   end
 
-  private
-
   def update(review, like)
     if review.update(like:)
       render status: :ok, json: review
     else
       render status: :internal_server_error, nothing: true
     end
+  end
+
+  def destroy(review)
+    if review.destroy
+      render status: :no_content, nothing: true
+    else
+      render status: :internal_server_error, nothing: true
   end
 end
