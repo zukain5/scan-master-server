1279111affe791c0b6b346f4b7350d3b9d6fc916
Kazuki ARAI <zukain5@gmail.com>
2023-09-14 14:39:29 +0900

    fix n+1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.all.includes(:orders)
     render :ok, json: @users, methods: :points
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find(params[:id]).includes(:orders)
     render :ok, json: @user, methods: :points
   end
 end
