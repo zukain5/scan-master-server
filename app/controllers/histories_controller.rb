567b099faed3c19d58ff38b45c7e7af02a86b65f
Kazuki ARAI <zukain5@gmail.com>
2023-09-14 17:50:20 +0900

    make histories#show created_at: :desc

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,7 +1,7 @@
 class HistoriesController < ApplicationController
   def show
     user = User.find(params[:user_id])
-    histories = Order.where(user:)
+    histories = Order.where(user:).order(created_at: :desc)
     render status: :ok, json: { 'orders': histories.as_json(include: :order_items) }
   end
 end
