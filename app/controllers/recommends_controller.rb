6e557cbbbcbc7b8ef621188b4397c83dc7307658
Kazuki ARAI <zukain5@gmail.com>
2023-09-14 11:21:11 +0900

    make recommend products user unique

--- app/controllers/recommends_controller.rb
+++ app/controllers/recommends_controller.rb
@@ -1,6 +1,7 @@
 class RecommendsController < ApplicationController
   def show
-    products = Product.where(is_ec: true).sample(4)
+    user = User.find(params[:user_id])
+    products = Product.where(is_ec: true).sample(4, random: Random.new(user.id))
     render status: :ok, json: products
   end
 end
