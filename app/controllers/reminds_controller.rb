236e2c966d943c4d743db5c33435fedc14a559a2
Kazuki ARAI <zukain5@gmail.com>
2023-09-15 14:12:34 +0900

    fix remind order

--- app/controllers/reminds_controller.rb
+++ app/controllers/reminds_controller.rb
@@ -2,7 +2,7 @@ class RemindsController < ApplicationController
   def show
     user = User.find(params[:user_id])
     orders = Order.where(user:)
-    items = OrderItem.where(order: orders).order(created_at: :desc)
+    items = OrderItem.where(order: orders)
     reminds = items.map do |item|
       next unless item.product.duration_days
 
@@ -11,7 +11,7 @@ class RemindsController < ApplicationController
         product_id: item.product.id,
         remind_at: item.created_at + item.product.duration_days.days,
       )
-    end.compact
+    end.compact.sort_by(&:remind_at).reverse
 
     render status: :ok, json: reminds
   end
