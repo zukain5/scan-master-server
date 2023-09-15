64bb1fee15d215719d2ea75e4b7e30355398a537
Kazuki ARAI <zukain5@gmail.com>
2023-09-15 16:15:39 +0900

    validates name presence true

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :orders, dependent: :nullify
   has_many :reviews, dependent: :destroy
 
+  validates :name, presence: true
+
   def points
     orders.sum(&:total_points)
   end
